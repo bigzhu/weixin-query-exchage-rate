e61f757d58dbd2cec0c32a6794a5c249ab6015a0
bigzhu <vermiliondun@gmail.com>
2017-02-24 13:22:06 +0800

    正确调用

--- exchage.py
+++ exchage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-参看 https://pay.weixin.qq.com/wiki/doc/api/external/jsapi_sl.php?chapter=9_15&index=10
+https://pay.weixin.qq.com/wiki/doc/api/external/jsapi_sl.php?chapter=9_15&index=10
 '''
 try:
     import requests
@@ -9,7 +9,25 @@ except ImportError:
     print 'you need run:'
     print 'sudo pip install requests'
     raise
-import hashlib
+from hashlib import md5
+import xmltodict
+
+import six
+
+
+def toText(value, encoding='utf-8'):
+    """将 value 转为 unicode，默认编码 utf-8
+
+    :param value: 待转换的值
+    :param encoding: 编码
+    """
+    if not value:
+        return ''
+    if isinstance(value, six.text_type):
+        return value
+    if isinstance(value, six.binary_type):
+        return value.decode(encoding)
+    return six.text_type(value)
 
 
 def dictToXml(params):
@@ -19,34 +37,42 @@ def dictToXml(params):
     for k, v in params.items():
         v = v.encode('utf8')
         k = k.encode('utf8')
-        xml += '<' + k + '>' + v + '</' + k + '>'
+        xml += '<%s><![CDATA[%s]]></%s>' % (k, v, k)
     xml += "</xml>"
     return xml
 
 
-def createSignature(params):
-    '''
-    http://mp.weixin.qq.com/wiki/17/2d4265491f12608cd170a95559800f2d.html#.E7.AC.AC.E4.BA.8C.E6.AD.A5.EF.BC.9A.E9.AA.8C.E8.AF.81.E6.9C.8D.E5.8A.A1.E5.99.A8.E5.9C.B0.E5.9D.80.E7.9A.84.E6.9C.89.E6.95.88.E6.80.A7
+def createSign(api_key, params):
+    """
+    create by bigzhu at 16/02/10 15:13:12 生成签名，用于微信支付
+    """
+    # 将键值对转为 key1=value1&key2=value2
+    key_az = sorted(params.keys())
+    pair_array = []
+    for k in key_az:
+        v = params.get(k, '').strip()
+        v = v.encode('utf8')
+        k = k.encode('utf8')
+        pair_array.append('%s=%s' % (k, v))
 
-    1. 将参数进行字典序排序
-    2. 将参数字符串拼接成一个字符串进行sha1加密
-    3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
-    '''
-    l_params = [v for v in params.values()]
-    l_params.sort()
-    sha1 = hashlib.sha1()
-    map(sha1.update, l_params)
-    signature = sha1.hexdigest()
-    return signature
+    stringA = '&'.join(pair_array)
+
+    stringSignTemp = stringA + '&key=' + api_key  # api_key, API密钥，需要在商户后台设置
+    return (md5(stringSignTemp).hexdigest()).upper()
 
 
-def query(appid, mch_id, fee_type, date, sub_mch_id=None):
+def query(appid, mch_id, fee_type, date, key, sub_mch_id=None):
     params = locals()
-    params['sign'] = createSignature(params)
+    if sub_mch_id is None:
+        del params['sub_mch_id']
+    key = params.pop('key')
+    # params['sign'] = createSign('Rlym2016YM9999999999999999999999', params)
+    params['sign'] = createSign(key, params)
     xml = dictToXml(params)
-    print xml
-    r = requests.post("https://api.mch.weixin.qq.com/pay/queryexchagerate", data=params)
-    print r.text
+    r = requests.post("https://api.mch.weixin.qq.com/pay/queryexchagerate", data=xml, stream=True)
+    text = r.raw.read()
+    print text
+    msg = xmltodict.parse(toText(text))['xml']
 
 if __name__ == '__main__':
-    query('wxa35c0aebbd19420f', '1375346702', 'THB', '20170223', '17138771')
+    pass
